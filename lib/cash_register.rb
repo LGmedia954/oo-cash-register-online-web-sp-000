83de33d2c80c525913454bcda50e67ed35a78674
Leslie G <lesliegonzalez@me.com>
2020-08-10 16:58:26 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,7 +9,15 @@ class CashRegister
   
   def apply_discount
     @discount.even? || @discount.to_f
+  end
   
+  def total
+    @total
+  end
+  
+  def add_item(title, price, quantity = 0)
+    @total += 1
+  end
   
   def self.all
     @@sales
